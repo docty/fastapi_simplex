4fcbe55bb46cd9c66f86d20ce677d64dced8781b
Kwasi Asiedu <createdarko@gmail.com>
2025-03-31 00:25:28 +0000

    release app

--- main.py
+++ main.py
@@ -1,15 +1,31 @@
 import uvicorn
 import numpy as np
 from fastapi import FastAPI
+from pydantic import BaseModel
+import joblib
 
 app = FastAPI()
 
+model = joblib.load('./model.pkl')
+class InputData(BaseModel):
+    feature: float
+
+class PredictionResult(BaseModel):
+    prediction: float
+
+
+@app.post('/predict', response_model=PredictionResult)
+def predict(input_data: InputData):
+
+    input_array = np.array([[input_data.feature]])
+
+    prediction = model.predict(input_array)
+
+    return {"prediction": prediction[0][0]}
 
 @app.get('/')
-async def root():
-    a = np.array([42])
-    print(a[0])
-    return {"message": a.tolist()}
+async def root(): 
+    return {"message": "Welcome to the FastAPI model prediction service!"}
 
 
 if __name__ == "__main__":
