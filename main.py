44168fe8ba09d360d5937674cd49e917080a7380
Kwasi Asiedu <createdarko@gmail.com>
2025-04-02 01:51:55 +0000

    update all codes

--- main.py
+++ main.py
@@ -5,21 +5,28 @@ from pydantic import BaseModel
  
 
 app = FastAPI()
+classifier = pipeline("zero-shot-classification", model="facebook/bart-large-mnli")
+
+candidate_labels = ["education", "health", "business", "sports", "politics", "entertainment"]
 
  
 class InputData(BaseModel):
-    feature: float
+    text: str
 
 class PredictionResult(BaseModel):
-    prediction: float
+    prediction: str
 
 
 @app.post('/predict', response_model=PredictionResult)
 def predict(input_data: InputData):
 
-    
+    result = classifier(input_data.text, candidate_labels)
+    scores = result['scores']
+    labels = result['labels'][0]
+    max_score_index = scores.index(max(scores))
 
-    return {"prediction": 32.9}
+
+    return {"prediction": labels}
 
 @app.get('/')
 async def root(): 
