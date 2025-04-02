a740c24e36e648f5eb7f54d9976b6d14546efb39
Kwasi Asiedu <createdarko@gmail.com>
2025-04-02 01:39:46 +0000

    update all codes

--- main.py
+++ main.py
@@ -1,13 +1,12 @@
 import uvicorn
-import numpy as np
+ 
 from fastapi import FastAPI
 from pydantic import BaseModel
-import onnxruntime as ort
+ 
 
 app = FastAPI()
 
-sess = ort.InferenceSession('./model.onnx')
-
+ 
 class InputData(BaseModel):
     feature: float
 
@@ -18,15 +17,9 @@ class PredictionResult(BaseModel):
 @app.post('/predict', response_model=PredictionResult)
 def predict(input_data: InputData):
 
-    input_array = np.array([[input_data.feature]], dtype=np.float32)
-
-    input_name = sess.get_inputs()[0].name
-    #output_name = sess.get_outputs()[0].name
-    prediction = sess.run(None, {input_name: input_array})[0]
-
     
 
-    return {"prediction": float(prediction[0][0])}
+    return {"prediction": 32.9}
 
 @app.get('/')
 async def root(): 
