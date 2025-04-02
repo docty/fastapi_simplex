edfb897aa54819828e7f6ae27ae31dadc44c94c8
Kwasi Asiedu <createdarko@gmail.com>
2025-04-02 02:04:04 +0000

    update all codes

--- main.py
+++ main.py
@@ -1,36 +1,31 @@
 import uvicorn
-from transformers import pipeline 
+ 
 from fastapi import FastAPI
 from pydantic import BaseModel
  
 
 app = FastAPI()
-classifier = pipeline("zero-shot-classification", model="facebook/bart-large-mnli")
-
-candidate_labels = ["education", "health", "business", "sports", "politics", "entertainment"]
 
  
 class InputData(BaseModel):
-    text: str
+    feature: float
 
 class PredictionResult(BaseModel):
-    prediction: str
+    prediction: float
 
 
 @app.post('/predict', response_model=PredictionResult)
 def predict(input_data: InputData):
 
-    #result = classifier(input_data.text, candidate_labels)
-    #scores = result['scores']
-    #labels = result['labels'][0]
-    #max_score_index = scores.index(max(scores))
+    # input_array = np.array([[input_data.feature]])
 
+    # prediction = model.predict(input_array)
 
-    return {"prediction": 'input_data'}
+    return {"prediction": 32.5}
 
 @app.get('/')
 async def root(): 
-    return {"message": "Everything you to the FastAPI model prediction service!"}
+    return {"message": "Welcome to the FastAPI model prediction service!"}
 
 
 if __name__ == "__main__":
